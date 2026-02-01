ef71ffc6aab77a97f099ce3d548e73b86e01b2d9
Laurent Ouma <lawiomosh3@gmail.com>
2026-02-01 19:21:18 +0300

    Add loading state to Submit button in AddFoundDocumentCase component

--- app/document-case/add-found.tsx
+++ app/document-case/add-found.tsx
@@ -159,6 +159,7 @@ const AddFoundDocumentCase = () => {
             text="Submit"
             suffixIcon={ArrowRight}
             onPress={form.handleSubmit(onSubmit)}
+            loading={uploadingFiles}
           />
         </VStack>
       </ScrollView>
