b8056003b4f27836a4230c7963d2bfef39ad5ecd
Laurent Ouma <lawiomosh3@gmail.com>
2026-02-02 16:30:35 +0300

    Implement submission confirmation actionsheet in CaseActions component, enhancing document verification process with error handling and loading state. Update button text and styles for improved user experience.

--- components/cases/CaseActions.tsx
+++ components/cases/CaseActions.tsx
@@ -1,3 +1,5 @@
+import { useDocumentCaseApi } from "@/hooks/use-document-cases";
+import { handleApiErrors } from "@/lib/api";
 import {
   CaseType,
   DocumentCase,
@@ -8,18 +10,31 @@ import dayjs from "dayjs";
 import { router } from "expo-router";
 import { ArrowRight } from "lucide-react-native";
 import React, { FC, useMemo } from "react";
-import { Button, ButtonIcon, ButtonText } from "../ui/button";
+import Toaster from "../toaster";
+import {
+  Actionsheet,
+  ActionsheetBackdrop,
+  ActionsheetContent,
+  ActionsheetDragIndicator,
+  ActionsheetDragIndicatorWrapper,
+} from "../ui/actionsheet";
+import { Button, ButtonIcon, ButtonSpinner, ButtonText } from "../ui/button";
+import { Heading } from "../ui/heading";
 import { Text } from "../ui/text";
+import { useToast } from "../ui/toast";
 import { VStack } from "../ui/vstack";
 
 type CaseActionsProps = {
   documentCase: DocumentCase;
 };
 const CaseActions: FC<CaseActionsProps> = ({ documentCase: docCase }) => {
+  const { submitDocumentCase } = useDocumentCaseApi();
+
   const dateFomart = "DD MMMM YYYY";
   const casetype: CaseType = docCase.foundDocumentCase ? "FOUND" : "LOST";
   const foundCase = docCase.foundDocumentCase;
   const lostCase = docCase.lostDocumentCase;
+  const toast = useToast();
   const canEdit = useMemo(() => {
     if (
       casetype === "FOUND" &&
@@ -36,6 +51,31 @@ const CaseActions: FC<CaseActionsProps> = ({ documentCase: docCase }) => {
 
     return false;
   }, [casetype, lostCase, foundCase]);
+  const [showActionsheet, setShowActionsheet] = React.useState(false);
+  const [submitting, setSubmitting] = React.useState(false);
+  const handleAcceptAndContinue = async () => {
+    setSubmitting(true);
+    try {
+      await submitDocumentCase(docCase.id);
+      setShowActionsheet(false);
+    } catch (error) {
+      const e = handleApiErrors(error);
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <Toaster
+            uniqueToastId={`toast-${id}`}
+            variant="outline"
+            title="Error submitting for review"
+            description={e.detail}
+            action="error"
+          />
+        ),
+      });
+    } finally {
+      setSubmitting(false);
+    }
+  };
   return (
     <VStack space="sm" className="pt-4">
       {canEdit && (
@@ -43,7 +83,7 @@ const CaseActions: FC<CaseActionsProps> = ({ documentCase: docCase }) => {
           size="lg"
           variant="solid"
           action="secondary"
-          className="rounded-full bg-gray-400 "
+          className="rounded-full bg-background-btn"
           onPress={() => {
             router.push({
               pathname: "/document-case/[caseId]/edit",
@@ -51,23 +91,71 @@ const CaseActions: FC<CaseActionsProps> = ({ documentCase: docCase }) => {
             });
           }}
         >
-          <ButtonText className="text-white">Edit Document</ButtonText>
+          <ButtonText className="text-white">
+            Review & Edit Document details
+          </ButtonText>
           <ButtonIcon as={ArrowRight} className="text-white" />
         </Button>
       )}
       {casetype === "FOUND" &&
         foundCase?.status === FoundDocumentCaseStatus.DRAFT && (
-          <Button
-            size="lg"
-            action="primary"
-            className="rounded-full bg-background-btn"
-            onPress={() => {}}
-          >
-            <ButtonText className="font-semibold text-white">
-              Submit for verification
-            </ButtonText>
-            <ButtonIcon as={ArrowRight} className="text-white" />
-          </Button>
+          <>
+            <Button
+              size="lg"
+              action="primary"
+              className="rounded-full bg-teal-500"
+              onPress={() => setShowActionsheet(true)}
+              disabled={submitting}
+            >
+              {submitting && <ButtonSpinner className="text-white" />}
+              <ButtonText className="font-semibold text-white">
+                Submit for verification
+              </ButtonText>
+              <ButtonIcon as={ArrowRight} className="text-white" />
+            </Button>
+            <Actionsheet
+              isOpen={showActionsheet}
+              onClose={() => {
+                // prevent closig intill submission is complete
+                if (!submitting) {
+                  setShowActionsheet(false);
+                }
+              }}
+            >
+              <ActionsheetBackdrop />
+              <ActionsheetContent>
+                <ActionsheetDragIndicatorWrapper>
+                  <ActionsheetDragIndicator />
+                </ActionsheetDragIndicatorWrapper>
+                <VStack className="w-full" space="lg">
+                  <Heading className="text-center">Confirm</Heading>
+                  <Text className="text-start text-typography-400 px-2 pb-2">
+                    By continuing, you confirm that the details you have entered
+                    are accurate.
+                    <Text className="font-semibold">
+                      After submitting, you will not be able to edit the
+                      document details.
+                    </Text>
+                    <Text>{"\n\n"}</Text>
+                    To verify this document, please either:
+                    {"\n"}- Drop the document at the nearest station for
+                    official verification, or
+                    {"\n"}- Request retrieval so a Citizen Link agent can come
+                    and collect it from you.
+                  </Text>
+                  <Button
+                    onPress={handleAcceptAndContinue}
+                    className="bg-background-btn rounded-full"
+                  >
+                    <ButtonText className="text-white">
+                      Confirm and submit
+                    </ButtonText>
+                    <ButtonIcon as={ArrowRight} className="text-white" />
+                  </Button>
+                </VStack>
+              </ActionsheetContent>
+            </Actionsheet>
+          </>
         )}
 
       <Text className="text-center text-typography-400 text-xs pt-4">
